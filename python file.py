12b7e193cff4d80bf08870413927e932ed78d3f1
SKSINGH36 <sugyansingh317@gmail.com>
2026-06-25 13:48:22 +0530

    Update python file.py

--- python file.py
+++ python file.py
@@ -1,40 +1,31 @@
-x = int(input("Enter your mark: "))
-print("My mark =", x)
+x = int(input("Enter your marks (out of 600): "))
 
-p = x / 600 * 100
-print("My percentage =", p)
+if 0 <= x <= 600:
+    print("My marks =", x)
 
-if p >= 90:
-    g = "A+"
+    p = (x / 600) * 100
+    print("My percentage = {:.2f}%".format(p))
+
+    if p >= 90:
+        g = "A+"
+    elif p >= 80:
+        g = "A"
+    elif p >= 70:
+        g = "B+"
+    elif p >= 60:
+        g = "B"
+    elif p >= 50:
+        g = "C+"
+    elif p >= 40:
+        g = "C"
+    elif p >= 30:
+        g = "D"
+    else:
+        g = "F"
+
     print("My Grade:", g)
 
-elif p >= 80 and p < 90:
-    g = "A"
-    print("My Grade:", g)
-
-elif p >= 70 and p < 80:
-    g = "B+"
-    print("My Grade:", g)
-
-elif p >= 60 and p < 70:
-    g = "B"
-    print("My Grade:", g)
-
-elif p >= 50 and p < 60:
-    g = "C+"
-    print("My Grade:", g)
-
-elif p >= 40 and p < 50:
-    g = "C"
-    print("My Grade:", g)
-
-elif p >= 30 and p < 40:
-    g = "D"
-    print("My Grade:", g)
-
-elif p >= 0 and p < 30:
-    g = "F"
-    print("This student is fail. Grade:", g)
-
+    if g == "F":
+        print("This student has failed.")
 else:
-    print("Invalid marks")
+    print("Invalid marks! Please enter marks between 0 and 600.")
